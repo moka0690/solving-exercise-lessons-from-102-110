1c6cd23e57385f0fdf31553f91c51f112d4a7622
moka0690 <madradz39@gmail.com>
2024-08-16 10:35:38 +0100

    Exercise solution  1

---
+++ ex1.js
@@ -0,0 +1,9 @@
+
+const PromptMasge = prompt("Print Number From – To", "Example: 5-20");
+const numbers = PromptMasge.split("-");
+const transformationToNumber = numbers.sort().map(Number);
+for ( let i = transformationToNumber[0] ;i<= transformationToNumber[1] ;i++ ){
+      console.log(i)
+}
+
+
