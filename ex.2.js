3b48bcd1658d55699ec73b3bbbce98257896d85f
moka0690 <madradz39@gmail.com>
2024-08-20 19:28:04 +0100

    Exercise solution  3

--- ex.2.js
+++ ex.2.js
@@ -1,11 +1,30 @@
 function createPopup() {
-    document.querySelector(".popup").style.display = 'block'
+  document.querySelector(".popup").style.display = "block";
 }
 function closePopup() {
-    document.querySelector(".popup").style.display = 'none';
+  document.querySelector(".popup").style.display = "none";
 }
-document.querySelector(".closeBtn").addEventListener("click", closePopup)
+document.querySelector(".closeBtn").addEventListener("click", closePopup);
 
-window.addEventListener("load",() => {
-    setTimeout(createPopup, 5000)
-})
+window.addEventListener("load", () => {
+  setTimeout(createPopup, 5000);
+});
+
+
+
+
+
+
+const counterDiv = document.querySelector(".counterDiv");
+const counter = parseInt(counterDiv.textContent);
+
+for (let i = counter; i >= 0; i--) {
+  setTimeout( ()=> {
+    counterDiv.textContent = i;
+  }, (counter - i) * 1000);
+}
+
+
+
+
+
